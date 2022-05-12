7f47580287a5fb821cda412b6d8425adcdd69508
Samuel <samnap11@gmail.com>
2022-05-13 04:39:28 +0700

    feat: utilize API to retrive storefront list

--- src/pages/stock.vue
+++ src/pages/stock.vue
@@ -1,12 +1,13 @@
-<script setup lang="ts">
-// TODO: Fetch data from endpoint
-import stock from '~/mock/stock.json'
-
-const stockData = stock.data
-</script>
 <template>
   <main class="mx-auto w-10/12 mt-4">
     <Header />
-    <Content :data="stockData" />
+    <Suspense>
+      <Content />
+      <template #fallback>
+        <h2 class="text-lg font-lato font-semibold tracking-wide leading-4 text-indigo-300">
+          Loading...
+        </h2>
+      </template>
+    </Suspense>
   </main>
 </template>
